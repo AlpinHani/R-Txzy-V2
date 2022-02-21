3c08bf2e97438b45c37123a7b60edacc35460a47
AlpinHani <99058202+AlpinHani@users.noreply.github.com>
2022-02-22 00:34:28 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (awa
 ┌〔 Donasi • Emoney 〕
 ├ GoPay: 085730903853 [AlyaXd]
 ├ Dana: 082221792667 [BOTCAHX]
-├ Pulsa: 081395861695 [BOTCAHX]
+├ Pulsa: 082287750102 [ALPINXD]
 └────
 `.trim(), watermark, 'Menu', '.menu', m)
 handler.help = ['donasi']
